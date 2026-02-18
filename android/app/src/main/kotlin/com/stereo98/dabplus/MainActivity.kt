d39617e7af016687447c042b0f5220877d6efd28
Davide Gialli <davidegialli@gmail.com>
2026-02-18 23:28:39 +0100

    fix black screen - MainActivity extends AudioServiceActivity

--- android/app/src/main/kotlin/com/stereo98/dabplus/MainActivity.kt
+++ android/app/src/main/kotlin/com/stereo98/dabplus/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.stereo98.dabplus
 
-import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: AudioServiceActivity()
