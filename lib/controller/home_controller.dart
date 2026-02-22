0ca778332c432857d7993c4982831b77759a31e2
Davide Gialli <davidegialli@gmail.com>
2026-02-22 22:39:20 +0100

    build 8: fix import Color in home_controller

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -4,6 +4,7 @@ import 'dart:math';
 
 import 'package:animate_icons/animate_icons.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:http/http.dart' as http;
